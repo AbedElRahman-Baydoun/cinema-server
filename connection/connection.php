0f62546859d5c62c3764595aff7e7c2a56fa9f79
abedb961 <abedbaydoun961@hotmail.com>
2025-06-30 19:06:37 +0300

    created users table

--- connection/connection.php
+++ connection/connection.php
@@ -4,8 +4,10 @@ $db_name = "cinema_db";
 $db_user = "root";
 $db_pass = null;
 
-$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
+} else {
+    echo "Connection successful!";
 }
 ?>
---
+++ migrations/001_create_users_table.php
@@ -0,0 +1,20 @@
+<?php 
+require("../connection/connection.php");
+
+
+$query = "CREATE TABLE users (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        email VARCHAR(100) NOT NULL UNIQUE,
+        mobile_number VARCHAR(20) NOT NULL UNIQUE,
+        password VARCHAR(255) NOT NULL,
+        age INT,
+        favorite_genre VARCHAR(50),
+        communication_preference ENUM('email', 'sms', 'none') DEFAULT 'email')";
+
+if ($conn->query($query) === TRUE) {
+    echo "Table users created successfully.";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+?>
