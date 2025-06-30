0f389aae4526b9323881a79e848072d2b419f156
abedb961 <abedbaydoun961@hotmail.com>
2025-06-30 15:38:17 +0300

    added a connection file

---
+++ connection/connection.php
@@ -0,0 +1,11 @@
+<?php 
+$db_host = "localhost";
+$db_name = "cinema_db";
+$db_user = "root";
+$db_pass = null;
+
+$mysqli = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
